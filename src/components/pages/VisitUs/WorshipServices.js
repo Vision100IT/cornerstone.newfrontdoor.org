f080da3dba2823120658c2b567f664b3351389dd
Bratalix <bratalix@gmail.com>
2018-07-19 15:45:19 +1000

    Content added

--- src/components/pages/VisitUs/WorshipServices.js
+++ src/components/pages/VisitUs/WorshipServices.js
@@ -34,7 +34,32 @@ class WorshipServices extends Component {
                     <div className="content">
                       <div className="node node-page clearfix">
                         <div className="content">
-                          <p>This page is still under construction.</p>
+                        <h1 className="header-lightBlue">Worship</h1>
+                        <p className="ministry-text">We seek to worship God in every part of life: hour to hour, day to day, and year after year.
+                        "So whether you eat or drink or whatever you do, do it all for the glory of God!" (1 Corinthians 10:31).<br/><br/>
+                        Our Sunday service is a special time of corporate worship.  We praise Jesus by singing, praying, reading and preaching His Word, and celebrating the sacraments of baptism and the Lord&apos;s Supper.</p>
+
+                        <h1 className="header-lightBlue">Our Services</h1>
+                        <p className="ministry-text">
+                          At <strong>Cornerstone Presbyterian</strong> we love Jesus and His Word the Bible, and we love to take His Word out to the city of Hobart.<br /><br />
+
+                          <strong>We absolutely love visitors and welcome you to our service on Sunday, 10am at 45 Melville Street. <a href="http://cornerstonehobart.com/contact/">(map)</a></strong><br /><br />
+
+                          <strong>We meet in Alfacrucis College and the entrance is set about 20m off the street. Look for the Flags.</strong><br /><br />
+
+                          There is limited free parking on site, and in the Red Cross car park over the road. There is also three hours of free parking in the Hobart Central car park off Melville Street.  (With $1/hour after that.)<br /><br />
+
+                          <strong>After you are greeted at the front door, head upstairs and take a seat.</strong><br /><br />
+
+                          Our church service run for about an hour and a quarter.  There are prayers, the Bible is read, and the Bible is explained in a message that lasts between 30-40 minutes.  We also sing songs of thanks
+                          and praise to our God for who He is and all that He has done for us.<br /><br />
+
+                          A crèche is run downstairs for babies and toddlers, and there is a <strong>Corner Pebble Sunday School</strong> class for children aged 5-11.  (All of our youth and children’s workers hold government “working with children” clearances.)<br /><br />
+
+                          Afterwards, morning tea is served with fresh tea and coffee.  This is a great time to meet new people, and catch up with old friends.<br /><br />
+
+                          Please feel free to ask questions, if your would like to speak to Pastor Campbell Markham, <a href="http://cornerstonehobart.com/contact/">you can contact him here.</a>
+                        </p>
                         </div>
 
 
