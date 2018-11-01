989da66ed5c8650655a375ff394905d21add049d
Bratalix <bratalix@gmail.com>
2018-11-01 16:07:13 +1100

    Adding mens ministry image to fix broken link

--- src/components/pages/GetInvolved/MensMinistry.js
+++ src/components/pages/GetInvolved/MensMinistry.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import React, { Component } from 'react';
+import MensMinistryImg from '../../../assets/ministry-men2.jpg';
 
 class MensMinistry extends Component {
   render() {
@@ -35,44 +36,44 @@ class MensMinistry extends Component {
                     <div className="content">
                       <div className="node node-page clearfix">
                         <div className="content">
-                        <div className="row">
-                          <div className=" col-md-4 col-xs-12">
-                          <p className="ministry-text" style={{fontWeight: "bold"}}>
-                            <em>Proverbs 29:23: "A man's pride brings him low, but a man of lowly spirit gains honor"</em>
-                          </p>
-                            <img src="http://cornerstonehobart.com/wp-content/uploads/2016/01/ministry-men2.jpg" width="258" height="196" />
+                          <div className="row">
+                            <div className=" col-md-4 col-xs-12">
+                              <p className="ministry-text" style={{ fontWeight: "bold" }}>
+                                <em>Proverbs 29:23: "A man's pride brings him low, but a man of lowly spirit gains honor"</em>
+                              </p>
+                              <img src={MensMinistryImg} width="258" height="196" />
 
-                          </div>
-                          <div className=" col-md-8 col-xs-12">
-                            <p className="ministry-text">
-                              Our men meet together for fellowship and for mutual support in their roles as leaders in their families and within the Church.
-                              A number of men’s breakfasts and other events are held throughout the year.
+                            </div>
+                            <div className=" col-md-8 col-xs-12">
+                              <p className="ministry-text">
+                                Our men meet together for fellowship and for mutual support in their roles as leaders in their families and within the Church.
+                                A number of men’s breakfasts and other events are held throughout the year.
                             </p>
-                            <br/>
-                            <p className="ministry-text">
-                            If you would like to be placed on the list to receive email notifications of Men’s Ministry functions,
+                              <br />
+                              <p className="ministry-text">
+                                If you would like to be placed on the list to receive email notifications of Men’s Ministry functions,
                             please email <a href="mailto:admin@cornerstonehobart.com">admin@cornerstonehobart.com</a>.
                             </p>
+                            </div>
                           </div>
                         </div>
-                        </div>
-
-
-
-                </div>
-                  </div>
-                </div>  </div>
-
-
-
-                        </div>
-
 
 
 
                       </div>
                     </div>
-                  </div>
+                  </div>  </div>
+
+
+
+              </div>
+
+
+
+
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
