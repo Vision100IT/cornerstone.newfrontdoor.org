0b58572581266ff35ccf3676e3c79b73a82ba42b
Bratalix <bratalix@gmail.com>
2019-06-13 12:39:54 +1000

    Disable notification

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ import './assets/css/custom.css';
 import './assets/css/events.css';
 import './assets/css/notification.css';
 
-import SiteNotification from './components/SiteNotification';
+//import SiteNotification from './components/SiteNotification';
 
 config.set(configuration);
 
@@ -65,13 +65,13 @@ class App extends Component {
           <Route exact path="/" component={HomePageWrapper} />
           <Route path="/:path" component={OtherPageWrapper} />
 
-          <Route
+          {/*<Route
             path={'*'}
             component={() => <SiteNotification
               showNotification={this.state.showNotification}
               onClose={this.onNotificationClose}
             />}
-          />
+            />*/}
 
           <Route path="*" component={Footer} />
         </div>
